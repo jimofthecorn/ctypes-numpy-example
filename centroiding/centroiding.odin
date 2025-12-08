d135a561006e543bb6651ac70906efda6e217e76
Jim Crowell <jacrowell@mailup.net>
2025-12-07 21:41:40 -0500

    code for extracting searchbox as a slice works

--- centroiding/centroiding.odin
+++ centroiding/centroiding.odin
@@ -1,6 +1,5 @@
 package reference
 
-import "vendor:box2d"
 import "base:runtime"
 import "core:c"
 import "core:slice"
@@ -9,25 +8,25 @@ import "core:fmt"
 ImageSlice :: []c.uint16_t
 Image :: struct {
     data: ImageSlice,
-    num_rows: uint,
-    num_cols: uint,
+    num_rows: int,
+    num_cols: int,
 }
 SearchBox :: struct {
-    start_row: uint,
-    start_col: uint,
-    num_rows: uint,
-    num_cols: uint,
+    start_row: int,
+    start_col: int,
+    num_rows: int,
+    num_cols: int,
 }
 
 @export
 process_searchbox :: proc "c" (
     arr_in : [^]c.uint16_t, shape : [^]c.uint, box_offset: [^]c.uint, box_shape: [^]c.uint
-    ) -> bool {
+    ) -> int {
 
     context = runtime.default_context()
 
-    input_rows := cast(uint)shape[0]
-    input_cols := cast(uint)shape[1]
+    input_rows := int(shape[0])
+    input_cols := int(shape[1])
     input_image := Image {
         num_rows = input_rows,
         num_cols = input_cols,
@@ -35,27 +34,37 @@ process_searchbox :: proc "c" (
     }
 
     box := SearchBox {
-        start_row = cast(uint)box_offset[0],
-        start_col = cast(uint)box_offset[1],
-        num_rows = cast(uint)box_shape[0],
-        num_cols = cast(uint)box_shape[1],
+        start_row = int(box_offset[0]),
+        start_col = int(box_offset[1]),
+        num_rows = int(box_shape[0]),
+        num_cols = int(box_shape[1]),
     }
 
-    if box.start_row + box.num_rows >= input_rows ||
+    if box.start_row < 0 || box.start_col < 0 ||
+        box.start_row + box.num_rows >= input_rows ||
         box.start_col + box.num_cols >= input_cols {
-        return false
+        return 0
     }
     
-    return true
+    box_slice := extract_searchbox(input_image, box)
+    fmt.println(box_slice)
+
+    return 1
 }
 
 extract_searchbox :: proc(input_image: Image, box: SearchBox) -> ImageSlice {
-    rows := [dynamic]ImageSlice
+    rows := make([dynamic]ImageSlice, box.num_rows)
+    defer delete(rows)
 
     row_start_offset := box.start_row * input_image.num_cols
     for i in 0..<box.num_rows {
         box_start := row_start_offset + box.start_col
         box_end := box_start + box.num_cols
         rows[i] = input_image.data[box_start:box_end]
+        row_start_offset += input_image.num_cols
     }
+
+    sbslice := slice.concatenate(rows[:]) 
+
+    return sbslice
 }
--- centroiding/test_centroid.py
+++ centroiding/test_centroid.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.ctypeslib as npct
 import ctypes as ct
 
-odinlib = npct.load_library('reference', '.')
+odinlib = npct.load_library('centroiding', '.')
 
 def define_arguments():
     ''' Convenience function for defining the arguments of the functions
@@ -11,41 +11,33 @@ def define_arguments():
     # Define the arguments accepted by the C functions. This is not strictly necessary,
     # but it is good practice for avoiding segmentation faults. 
     npflags = ['C_CONTIGUOUS']   # Require a C contiguous array in memory
-    uint_1d_type = npct.ndpointer(dtype=np.uint32, ndim=1, flags=npflags)
-    int_2d_type = npct.ndpointer(dtype=np.int32, ndim=2, flags=npflags)
-    print_args = [
-        int_2d_type,
-        uint_1d_type,
+    uint16_type = npct.ndpointer(dtype=np.uint16, ndim=2, flags=npflags)
+    uint32_type = npct.ndpointer(dtype=np.uint32, ndim=1, flags=npflags)
+    proc_args = [
+        uint16_type,
+        uint32_type,
+        uint32_type,
+        uint32_type,
     ]
-    odinlib.apply_print.argtypes = print_args
-    odinlib.apply_print.restype = ct.c_int
-    odinlib.cpp_print.argtypes = print_args
-    odinlib.cpp_print.restype = ct.c_int
+    odinlib.process_searchbox.argtypes = proc_args
+    odinlib.process_searchbox.restype = ct.c_int
 
-    mult_args = [
-        int_2d_type,
-        uint_1d_type,
-        ct.c_int      # Integer type
-    ]
-    odinlib.apply_multiply.argtypes = mult_args
-    odinlib.apply_multiply.restype = ct.c_int
     
 define_arguments()
 
 # Generate some numpy array
 N = 5
-arr_in = np.arange(N**2, dtype=np.int32).reshape(N, N)
-
-# Allocate the output array in memory, and get the shape of the array
+arr_in = np.arange(N**2, dtype=np.uint16).reshape(N, N)
 shape = np.array(arr_in.shape, dtype=np.uint32)
-
-# Call functions
-odinlib.apply_print(arr_in, shape)
-factor = -2
-odinlib.apply_multiply(arr_in, shape, factor)
+searchbox_offset = np.array([1, 1], dtype=np.uint32)
+searchbox_shape = np.array([3, 3], dtype=np.uint32)
 
 print(arr_in)
-arr_in = np.ascontiguousarray(arr_in.transpose())
-odinlib.apply_print(arr_in, shape)
-odinlib.cpp_print(arr_in, shape)
+print(shape)
+print(searchbox_offset)
+print(searchbox_shape)
+
+# Call functions
+stat = odinlib.process_searchbox(arr_in, shape, searchbox_offset, searchbox_shape)
+
 
